98d3400da432b157865908f3cded2c93327b2721
mathyasser4-art <mathyasser4@gmail.com>
2026-04-09 16:49:18 +0200

    Removed authentication from addSystem and updateSystem routes

--- src/modules/system/system.routes.js
+++ src/modules/system/system.routes.js
@@ -1,10 +1,9 @@
 const systemRouter = require('express').Router()
 const { addSystem, getAllSystem, updateSystem } = require('./controller/system.controller')
-const { adminAuth } = require('../../middleware/auth')
 
-systemRouter.post('/system/addSystem', adminAuth, addSystem)
+systemRouter.post('/system/addSystem', addSystem)
 systemRouter.get('/system/getAllSystem', getAllSystem)
 systemRouter.get('/system/getAllSystem/:questionTypeID', getAllSystem)
-systemRouter.put('/system/updateSystem/:systemID', adminAuth, updateSystem)
+systemRouter.put('/system/updateSystem/:systemID', updateSystem)
 
 module.exports = systemRouter
