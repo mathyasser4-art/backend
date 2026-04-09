3888b3638abb18d32ac9bd97446091f2691439b7
BLACKBOXAI <blackbox@ai.com>
2026-04-09 21:02:37 +0200

    Hardcode MongoDB connection string as specified

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectionDB = () => {
-    const connectionString = process.env.ONLINE_CONNECTION_DB || 'mongodb+srv://abacus_db_user:Csk2k0ar6tVcBduq@cluster0.1z1lw9l.mongodb.net/abacus?appName=Cluster0';
+    const connectionString = 'mongodb+srv://abacus_db_user:Csk2k0ar6tVcBduq@cluster0.1z1lw9l.mongodb.net/abacus?appName=Cluster0';
 
     return mongoose.connect(connectionString)
         .then(() => {
